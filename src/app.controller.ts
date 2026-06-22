f4cd240f240ef484595c0020c25dae81ad52d5c1
AnrresJurado <andrsflp68@hotmail.com>
2026-06-22 11:46:39 -0500

    feat:add swagger docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger'; // <-- Importa esto
 
+@ApiTags('Principal') // <-- Ponle una etiqueta a tu controlador base
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Endpoint de bienvenida por defecto' }) // <-- Describe la ruta
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuración base de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Mi Primer API en NestJS')
+    .setDescription('Documentación de prueba en un proyecto vacío')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); // Se servirá en /docs
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
